1c14cfff2ee6ea29ce5ab524cc287da5a64fa126
Laetitia Deschamps <name>
2021-09-02 08:26:52 +0200

    login-component

--- assets/App.vue
+++ assets/App.vue
@@ -2,13 +2,19 @@
     <div>
         <h2>Welcome home</h2>
         <button @click="counter++"> Click me {{counter}} </button>
+        <Register />
     </div>
-    
+
 </template>
 
 <script>
+import Register from './components/register';
 
 export default {
+    name: 'App',
+    components: {
+        Register
+    },
     data: ()=> ({
         counter: 0
     })
