926a93ec3a13567ece88357da4737af609c27a6c
Laetitia Deschamps <name>
2021-09-03 13:08:19 +0200

    added unique entity constraint email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,9 +9,12 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use App\Validator as CustomValidator;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity("email", message="Ce mail est déjà utilisé")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
